3f7cc2999001a0a7d93f17fc38c91f56c5903f79
Joel Shooster <JBShooster@gmail.com>
2016-05-10 11:15:13 -0700

    Completed all exercises in HW 1

--- hw1/cipher.py
+++ hw1/cipher.py
@@ -5,15 +5,21 @@
 text = raw_input("Enter message: ")
 letters = "abcdefghijklmnopqrstuvwxyz"
 upper_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-c = 3
-cipher = ''
+numbers = "0123456789"
+#This is where we set the shift value. Right now it's set to 4.
+c = 4
+cipher = ""
 
 for x in text:
   if x in letters:
     cipher += letters[(letters.index(x)+c)%(len(letters))]
   elif x in upper_letters:
     cipher += upper_letters[(upper_letters.index(x)+c)%(len(upper_letters))]
+  elif x in numbers:
+    x = numbers[(numbers.index(x)+4)%(len(numbers))]
+    x = int(x) % 6
+    x = str(x)
+    cipher += x
   elif x == " ":
     cipher += " "
-
 print("Your message: " + cipher)
